7c4867c7d11a4b77ffcad625662396bc4fa7e2b7
Trevor Leffert <leffertt@gmail.com>
2026-09-09 16:29:57 -0500

    added playerstore method to toggle partner commander and trigger creation if needed

--- src/app/state/store/with-players.ts
+++ src/app/state/store/with-players.ts
@@ -1,6 +1,8 @@
-import { patchState, signalStoreFeature, type, withMethods } from '@ngrx/signals';
-import { addEntity, entityConfig, updateEntity, withEntities } from '@ngrx/signals/entities';
+import { patchState, signalStoreFeature, type, withMethods, } from '@ngrx/signals';
+import { addEntity, entityConfig, EntityState, updateEntity, withEntities } from '@ngrx/signals/entities';
 import { DEFAULT_PLAYER_STATE, Player } from '../models/player';
+import { Signal } from '@angular/core';
+import { Commander } from '../models/commander';
 
 export const playerConfig = entityConfig({
     entity: type<Player>(),
@@ -13,8 +15,15 @@ export function addPlayerEntity(playerId: Player['id']) {
 
 export function withPlayers() {
     return signalStoreFeature(
+        {
+            methods: type<{addPartnerCommander(playerId: Player['id']): void}>(),
+            props: type<{
+                commandersByOwnerId: Signal<Record<string, Commander[]>>;
+            }>()
+        },
         withEntities(playerConfig),
-        withMethods((store) => ({
+        withMethods((store) => ({            
+            
             incrementHealth(playerId: Player['id'], amount: number): void {
                 patchState(
                     store,
@@ -39,6 +48,29 @@ export function withPlayers() {
                     ),
                 );
             },
+            togglePartner(playerId: Player['id']): void {
+                let player = store.entityMap()[playerId];
+                if (!player) {
+                    return;
+                }
+                const playerCommanders = store.commandersByOwnerId()[playerId];
+                const willPartnerBeEnabled = !player.partnerEnabled;
+
+                patchState(
+                    store,
+                    updateEntity(
+                        {
+                            id: playerId,
+                            changes: () => ({ partnerEnabled: willPartnerBeEnabled }),
+                        },
+                        playerConfig,
+                    )
+                );
+                
+                if (willPartnerBeEnabled && playerCommanders.length < 2) {
+                    store.addPartnerCommander(playerId);
+                } 
+            }
         })),
     );
 }
